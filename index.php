af819dc881157866d0aae633dce6b19cd8320ee1
Alicia Ramirez <aramirez@rrc.ca>
2017-09-01 11:48:03 -0500

    Initial theme files added
    Starting point from original theme ZIP.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Enter Title Here</title>
+
+    <?php wp_head(); ?>
+
+</head>
+<body>
+Nothing here
+
+<?php wp_footer(); ?>
+</body>
+</html>
